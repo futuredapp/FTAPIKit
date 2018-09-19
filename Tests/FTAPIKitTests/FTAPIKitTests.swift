803f258c620d85a314d46db8017c765ba5bfefcd
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2018-09-19 14:13:50 +0200

    Generate SwiftPlate project

---
+++ Tests/FTAPIKitTests/FTAPIKitTests.swift
@@ -0,0 +1,23 @@
+//
+//  FTAPIKitTests.swift
+//  FUNTASTY Digital s.r.o.
+//
+//  Created by Matěj Kašpar Jirásek on 19/09/2018.
+//  Copyright © 2018 FUNTASTY Digital s.r.o.. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import FTAPIKit
+
+class FTAPIKitTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(FTAPIKit().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
