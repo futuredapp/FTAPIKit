85f2a2f5430d2a1191037c8ed90b4c62103cfb45
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2019-01-03 11:07:47 +0100

    Do not add query when no parameters are present

--- Sources/URL+APIAdapter.swift
+++ Sources/URL+APIAdapter.swift
@@ -16,6 +16,9 @@ extension URL {
     }
 
     func appendingQuery(parameters: [String: String]) -> URL {
+        guard !parameters.isEmpty else {
+            return self
+        }
         var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
         let oldItems = components?.queryItems ?? []
         components?.queryItems = oldItems + parameters.map(URLQueryItem.init)
