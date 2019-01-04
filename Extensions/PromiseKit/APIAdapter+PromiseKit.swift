709f4d2bda7bfc15cbe96d28f495e731eae4682f
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2019-01-04 16:15:30 +0100

    Remove cancellable promise

--- Extensions/PromiseKit/APIAdapter+PromiseKit.swift
+++ Extensions/PromiseKit/APIAdapter+PromiseKit.swift
@@ -1,32 +1,30 @@
 //
-//  PromiseAPIAdapter.swift
-//  Gastromapa
+//  APIAdapter+PromiseKit.swift
+//  FTAPIKit
 //
-//  Created by Adam Salih on 12/09/2018.
+//  Created by Matěj Jirásek on 03/01/2019.
 //  Copyright © 2018 FUNTASTY Digital s.r.o. All rights reserved.
 //
 
 import PromiseKit
 
-public struct CancellablePromise<T> {
-    let promise: Promise<T>
-    let trigger: APIAdapter.CancellationTrigger?
-}
-
 extension APIAdapter {
     public func request<Endpoint: APIResponseEndpoint>(response endpoint: Endpoint) -> Promise<Endpoint.Response> {
-        return request(response:endpoint).promise
+        return Promise<Endpoint.Response> { resolver in
+            request(response: endpoint) { result in
+                switch result {
+                case .value(let value):
+                    resolver.fulfill(value)
+                case .error(let error):
+                    resolver.reject(error)
+                }
+            }
+        }
     }
 
     public func request(data endpoint: APIEndpoint) -> Promise<Data> {
-        return request(data:endpoint).promise
-    }
-
-    @discardableResult
-    public func request<Endpoint: APIResponseEndpoint>(response endpoint: Endpoint) -> CancellablePromise<Endpoint.Response> {
-        var trigger: CancellationTrigger? = nil
-        let promise = Promise<Endpoint.Response> { resolver in
-            trigger = request(response: endpoint) { result in
+        return Promise<Data> { resolver in
+            request(data: endpoint) { result in
                 switch result {
                 case .value(let value):
                     resolver.fulfill(value)
@@ -35,24 +33,5 @@ extension APIAdapter {
                 }
             }
         }
-
-        return CancellablePromise(promise: promise, trigger: trigger)
-    }
-
-    @discardableResult
-    public func request(data endpoint: APIEndpoint) -> CancellablePromise<Data> {
-        var trigger: CancellationTrigger? = nil
-        let promise = Promise<Data> { resolver in
-            trigger = request(data: endpoint) { result in
-                switch result {
-                case .value(let value):
-                    resolver.fulfill(value)
-                case .error(let error):
-                    resolver.reject(error)
-                }
-            }
-        }
-
-        return CancellablePromise(promise: promise, trigger: trigger)
     }
 }
