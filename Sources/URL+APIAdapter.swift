2a6e8750d249566f65d58b11c28f6ef9dd3d98f4
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2019-02-27 15:54:24 +0100

    Add MIME type and file size extension to URLs

--- Sources/URL+APIAdapter.swift
+++ Sources/URL+APIAdapter.swift
@@ -6,9 +6,13 @@
 //  Copyright © 2018 FUNTASTY Digital s.r.o. All rights reserved.
 //
 
-import struct Foundation.URL
-import struct Foundation.URLComponents
-import struct Foundation.URLQueryItem
+import Foundation
+
+#if os(iOS) || os(watchOS) || os(tvOS)
+import MobileCoreServices
+#elseif os(macOS)
+import CoreServices
+#endif
 
 extension URL {
     mutating func appendQuery(parameters: [String: String]) {
@@ -23,5 +27,20 @@ extension URL {
         let oldItems = components?.queryItems ?? []
         components?.queryItems = oldItems + parameters.map(URLQueryItem.init)
         return components?.url ?? self
+    }
+
+    var mimeType: String {
+        if let id = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension as CFString, nil)?.takeRetainedValue(), let contentType = UTTypeCopyPreferredTagWithClass(id, kUTTagClassMIMEType)?.takeRetainedValue() {
+            return contentType as String
+        }
+        return "application/octet-stream"
+    }
+
+    func contentLength() throws -> Int64 {
+        let attributes = try FileManager.default.attributesOfItem(atPath: path)
+        guard let size = attributes[FileAttributeKey.size] as? Int64 else {
+            throw APIError.uploadFileNotLoaded
+        }
+        return size
     }
 }
