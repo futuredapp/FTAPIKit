155441cb1c69b4d21dc186178d75901ee8c9d0be
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2019-02-28 15:22:36 +0100

    Increase output stream size to one page

--- Sources/OutputStream+Write.swift
+++ Sources/OutputStream+Write.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 extension OutputStream {
-    private static let streamBufferSize = 1024
+    private static let streamBufferSize = 4096
 
     func write(inputStream: InputStream) throws {
         inputStream.open()
