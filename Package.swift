fb73c54f30b94542647e825c8b36ebaad3bdd101
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2020-01-29 14:47:32 +0100

    Remove PromiseKit extension

--- Package.swift
+++ Package.swift
@@ -8,20 +8,11 @@ let package = Package(
         .library(
             name: "FTAPIKit",
             targets: ["FTAPIKit"]),
-        .library(
-            name: "FTAPIKitPromiseKit",
-            targets: ["FTAPIKitPromiseKit"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.8.4")
     ],
     targets: [
         .target(
             name: "FTAPIKit",
             dependencies: []),
-        .target(
-            name: "FTAPIKitPromiseKit",
-            dependencies: ["FTAPIKit", "PromiseKit"]),
         .testTarget(
             name: "FTAPIKitTests",
             dependencies: ["FTAPIKit"])
