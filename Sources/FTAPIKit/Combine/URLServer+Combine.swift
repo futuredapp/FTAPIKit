84f3926fe0c96d5dbf1002f4261a76deef17e9c6
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2021-01-26 11:53:34 +0100

    Make endpoint publisher internal

--- Sources/FTAPIKit/Combine/URLServer+Combine.swift
+++ Sources/FTAPIKit/Combine/URLServer+Combine.swift
@@ -4,28 +4,32 @@ import Combine
 
 @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
 public extension URLServer {
-    func publisher(endpoint: Endpoint) -> Publishers.Endpoint<Void, ErrorType> {
+    func publisher(endpoint: Endpoint) -> AnyPublisher<Void, ErrorType> {
         Publishers.Endpoint { completion in
             self.call(endpoint: endpoint, completion: completion)
         }
+        .eraseToAnyPublisher()
     }
 
-    func publisher(data endpoint: Endpoint) -> Publishers.Endpoint<Data, ErrorType> {
+    func publisher(data endpoint: Endpoint) -> AnyPublisher<Data, ErrorType> {
         Publishers.Endpoint { completion in
             self.call(data: endpoint, completion: completion)
         }
+        .eraseToAnyPublisher()
     }
 
-    func publisher<EP: ResponseEndpoint>(response endpoint: EP) -> Publishers.Endpoint<EP.Response, ErrorType> {
+    func publisher<EP: ResponseEndpoint>(response endpoint: EP) -> AnyPublisher<EP.Response, ErrorType> {
         Publishers.Endpoint { completion in
             self.call(response: endpoint, completion: completion)
         }
+        .eraseToAnyPublisher()
     }
 
-    func publisher(download endpoint: Endpoint) -> Publishers.Endpoint<URL, ErrorType> {
+    func publisher(download endpoint: Endpoint) -> AnyPublisher<URL, ErrorType> {
         Publishers.Endpoint { completion in
             self.download(endpoint: endpoint, completion: completion)
         }
+        .eraseToAnyPublisher()
     }
 }
 
