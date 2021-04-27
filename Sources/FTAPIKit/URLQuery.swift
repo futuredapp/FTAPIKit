d1ac6f87f817e14ceb7dfbab639b891066d8f0e1
Patrik Potocek <patrik.potocek@futured.app>
2021-04-27 14:08:32 +0200

    Remove manual escaping of query items

--- Sources/FTAPIKit/URLQuery.swift
+++ Sources/FTAPIKit/URLQuery.swift
@@ -8,13 +8,7 @@ public struct URLQuery: ExpressibleByDictionaryLiteral {
     }
 
     public init(items: [(String, String)]) {
-        self.items = items.compactMap { key, value in
-            guard let encodedKey = key.addingPercentEncoding(withAllowedCharacters: .urlQueryNameValueAllowed),
-                let encodedValue = value.addingPercentEncoding(withAllowedCharacters: .urlQueryNameValueAllowed) else {
-                    return nil
-            }
-            return URLQueryItem(name: encodedKey, value: encodedValue)
-        }
+        self.items = items.map(URLQueryItem.init)
     }
 
     public init(dictionaryLiteral elements: (String, String)...) {
