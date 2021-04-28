02a400abbf658126982395fe22ad46eaf50a1718
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2021-04-28 11:45:20 +0200

    Add manual encoding to URL query

--- Sources/FTAPIKit/URLQuery.swift
+++ Sources/FTAPIKit/URLQuery.swift
@@ -1,17 +1,33 @@
 import Foundation
 
 public struct URLQuery: ExpressibleByDictionaryLiteral {
-    let items: [URLQueryItem]
+    private let items: [URLQueryItem]
 
     init() {
         self.items = []
     }
 
-    public init(items: [(String, String)]) {
-        self.items = items.map(URLQueryItem.init)
+    public init(items: [URLQueryItem]) {
+        self.items = items
     }
 
     public init(dictionaryLiteral elements: (String, String)...) {
-        self.init(items: elements)
+        self.init(items: elements.map(URLQueryItem.init))
+    }
+
+    private func encode(item: URLQueryItem) -> URLQueryItem? {
+        let encodedName = item.name.addingPercentEncoding(withAllowedCharacters: .urlQueryNameValueAllowed) ?? item.name
+        let encodedValue = item.value?.addingPercentEncoding(withAllowedCharacters: .urlQueryNameValueAllowed)
+        return URLQueryItem(name: encodedName, value: encodedValue)
+    }
+
+    var percentEncoded: String? {
+        guard !items.isEmpty else {
+            return nil
+        }
+        return items.lazy
+            .compactMap(encode)
+            .map { item in "\(item.name)=\(item.value ?? String())" }
+            .joined(separator: "&")
     }
 }
