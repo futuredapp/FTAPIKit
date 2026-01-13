2289ca8ebbdb6ac4d15ab55d0d1915b7be4723b6
Jakub Marek <jakubmarek@icloud.com>
2026-01-13 20:05:57 +0100

    Add files via upload

---
+++ Templates/Endpoints.xctemplate/HasRequest/___VARIABLE_endpointName___Endpoints.swift
@@ -0,0 +1,10 @@
+//  ___FILEHEADER___
+
+import FTAPIKit
+
+struct ___VARIABLE_templateName___Endpoint: APIRequestEndpoint {
+    typealias Request = ___VARIABLE_templateName___Request
+
+    var body: ___VARIABLE_templateName___Request
+    var path: String
+}
---
+++ Templates/Endpoints.xctemplate/HasResponse/___VARIABLE_endpointName___Endpoints.swift
@@ -0,0 +1,9 @@
+//  ___FILEHEADER___
+
+import FTAPIKit
+
+struct ___VARIABLE_templateName___Endpoint: APIResponseEndpoint {
+    typealias Response = ___VARIABLE_templateName___Response
+
+    let path: String = ""
+}
